71529e6ace966c60f099766a7750da9f9d4b4442
guoyw-nju <guoyw@qq.com>
2021-10-29 22:06:01 +0800

    修改参数使得模型适用于hap-big的场景，添加了注释

--- background-subtractor/background-subtractor.py
+++ background-subtractor/background-subtractor.py
@@ -1,13 +1,13 @@
 import cv2
 import heapq
 
-vedio = r'.\data\hap-small-2.mp4'
+vedio = r'.\data\hap-big.mp4'
 videoroute = r'.\output'
 
 cap = cv2.VideoCapture(vedio)
 
 # 提高history, 提高var可以减弱闪烁效应
-fgbg = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=50, detectShadows=False)
+fgbg = cv2.createBackgroundSubtractorMOG2(history=800, varThreshold=100, detectShadows=False)
 
 cv2.namedWindow('frame', 0)
 cv2.resizeWindow('frame', 576*2, 324*2)
@@ -21,7 +21,8 @@ while True:
     if not ret:
         break
 
-    fgmask = fgbg.apply(frame)
+    # learningRate决定了背景模型学习的速度，取值0~1，取值越大表示背景更新越快
+    fgmask = fgbg.apply(frame, learningRate=0.1)
     fgmask = cv2.erode(fgmask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)), iterations=1)
     fgmask = cv2.dilate(fgmask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)), iterations=2)
 
@@ -34,7 +35,8 @@ while True:
     # contours_index = heapq.nlargest(10, contours_square)
     contours = [c for c in contours if cv2.contourArea(c) > 10]
     for c in contours:
-        if len(contours) < 50:
+        # 这个判断是为了忽略出现闪烁而识别出多个contours的情况
+        if len(contours) < 100:
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
